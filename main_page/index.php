629e77021dabed5b95a8bb74246b2b891b336cd5
Ana Colaco <136480297+anacolaco07@users.noreply.github.com>
2023-08-29 01:28:32 -0300

    Delete main_page directory

--- main_page/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../Config.php";
-echo "<a href='../prontuario/prontuario_form.php'>Novo Prontuário</a><br/>";
-echo "<a href='../perfil/editar_form.php'>Perfil</a><br/>";
-echo "<a href='../login/login.php'>Sair</a><br/>";
-
-
-?>
