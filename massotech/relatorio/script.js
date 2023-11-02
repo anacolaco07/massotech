8ecb6df802728283e0d9913793c6edcca6334201
Ana Colaco <136480297+anacolaco07@users.noreply.github.com>
2023-11-02 15:30:38 -0300

    Delete massotech directory

--- massotech/relatorio/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#chkveg').multiselect({ includeSelectAllOption: true });
-
-    $('#btnget').click(function() {
-        const selectedValues = $('#chkveg').val() || [];
-        const selectedValuesText = selectedValues.join(', ');
-        $('#selected-values').text('Valores Selecionados: ' + selectedValuesText);
-    });
-});
